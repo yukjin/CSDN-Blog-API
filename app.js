741cc20409ccdba67f829dfd145d67be6e3948c0
yukjin <luzhen@sungymobile.com>
2014-10-31 11:00:39 +0800

    add get /

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var app = express();
 app.get('/getArticleList', function (req,res) {
   BlogAPIUtils.getArticleList(req,res);
 });
+app.get('/', function (req,res) {
+    res.send('目前支持以下API:<br/>'+'getArticleList<br/>'+'getArticleCategory');
+})
 app.get('/getArticleCategory', function (req,res) {
     BlogAPIUtils.getArticleCategory(req,res);
 })
